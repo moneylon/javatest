89ac94e551976dae8bcb32a0ceef909119692d5c
ben <bencool1125@gmail.com>
2018-10-25 17:40:38 +0800

    Initial commit

--- src/test/java/testsort.java
+++ src/test/java/testsort.java
@@ -9,12 +9,17 @@ public class testsort {
 	@Test
 	public void test() {
 		sort Sort = new sort();
+		/*test insertionsort*/
 		assertArrayEquals(new int[] {0,0,0,0,0},sort.InsertionSort(new int[] {0,0,0,0,0}));
 		assertArrayEquals(new int[] {1,2,3,4,5},sort.InsertionSort(new int[] {5,4,3,2,1}));
 		assertArrayEquals(new int[] {1,2,3,4,5},sort.InsertionSort(new int[] {1,2,3,4,5}));
 		assertArrayEquals(new int[] {1,2,3,4,5},sort.InsertionSort(new int[] {3,2,1,4,5}));
 		assertArrayEquals(new int[] {-5,1,2,3,4},sort.InsertionSort(new int[] {-5,2,4,3,1}));
 		assertArrayEquals(new int[] {-5,-4,-3,-2,-1},sort.InsertionSort(new int[] {-1,-5,-4,-3,-2}));
+		/*test quicksort*/
+		assertArrayEquals(new int[] {1,2,3,4,5},sort.qsort(new int[]{3,2,1,4,5},0,4));
+		assertArrayEquals(new int[] {0,0,0,0,0},sort.qsort(new int[]{0,0,0,0,0},0,4));
+		assertArrayEquals(new int[] {-999,-888,-777,-666,-555},sort.qsort(new int[]{-777,-555,-666,-888,-999},0,4));
 		
 	}
 
